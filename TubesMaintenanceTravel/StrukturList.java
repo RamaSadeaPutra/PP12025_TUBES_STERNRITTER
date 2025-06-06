3d763599b554a8805d3a73dba247ad67465ef1e0
Rama Sadea Putra <145627033+RamaSadeaPutra@users.noreply.github.com>
2025-06-06 23:38:09 +0700

    Update StrukturList.java

--- TubesMaintenanceTravel/StrukturList.java
+++ TubesMaintenanceTravel/StrukturList.java
@@ -1,4 +1,4 @@
-package MaintenanceTravel;
+package TubesMaintenanceTravel;
 
 
 public class StrukturList {
@@ -49,6 +49,71 @@ public class StrukturList {
                 curNode.setNext(newNode);
             }
         }
+
+         public void removeHead() {
+        if (HEAD == null) {
+            System.out.println("List kosong.");
+        } else {
+            Node temp = HEAD;
+            HEAD = HEAD.getNext();
+            dispose(temp);
+            System.out.println("Data di head berhasil dihapus.");
+        }
     }
 
+    public void removeTail() {
+        if (HEAD == null) {
+            System.out.println("List kosong.");
+        } else if (HEAD.getNext() == null) { 
+            dispose(HEAD);
+            HEAD = null;
+            System.out.println("Satu-satunya elemen berhasil dihapus.");
+        } else {
+            Node curNode = HEAD;
+            Node prevNode = null;
+
+            while (curNode.getNext() != null) {
+                prevNode = curNode;
+                curNode = curNode.getNext();
+            }
+
+            prevNode.setNext(null);
+            dispose(curNode);
+            System.out.println("Data di tail berhasil dihapus.");
+        }
+    }
+
+    public void removeMid(int posisi) {
+        if (HEAD == null) {
+            System.out.println("List kosong.");
+        } else if (posisi <= 1) {
+            removeHead();
+        } else {
+            Node curNode = HEAD;
+            Node prevNode = null;
+            int index = 1;
+
+            while (curNode != null && index < posisi) {
+                prevNode = curNode;
+                curNode = curNode.getNext();
+                index++;
+            }
+
+            if (curNode == null) {
+                System.out.println("Posisi melebihi panjang list atau tidak valid.");
+            } else {
+                prevNode.setNext(curNode.getNext());
+                dispose(curNode);
+                System.out.println("Data di posisi " + posisi + " berhasil dihapus.");
+            }
+        }
+    }
+
+
+
+
+        
+    }
+
+    
    
